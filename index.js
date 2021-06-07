7d2707c0d868927fc40a4ccbd1c5a09699df52dd
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2021-06-07 19:42:27 +0700

    gg

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+window.addEventListener("contextmenu", e => e.preventDefault());
+
 // PASTE YOUR API HERE
 let api = "";
 
@@ -70,6 +72,8 @@ class Beatmap {
         this.metadata = document.createElement("div");
         this.difficulty = document.createElement("div");
         this.stats = document.createElement("div");
+        this.mods = document.createElement("div");
+        this.pickedStatus = document.createElement("div");
 
         this.bg.id = this.layerName;
         this.map.id = `${this.layerName}BG`;
@@ -77,13 +81,19 @@ class Beatmap {
         this.metadata.id = `${this.layerName}META`;
         this.difficulty.id = `${this.layerName}DIFF`;
         this.stats.id = `${this.layerName}Stats`;
+        this.mods.id = `${this.layerName}Mods`;
+        this.pickedStatus.id = `${this.layerName}STATUS`;
 
-        this.metadata.style.cssText = `position: absolute; top: 30px; left: 20px; width: 500px; color: #fff; font-family: Exo2; font-size: 15px; line-height: 30px; text-shadow: 0 2px 3px black;`;
-        this.difficulty.style.cssText = `position: absolute; top: 50px; left: 20px; width: 500px; color: #fff; font-family: Exo2; font-size: 15px; line-height: 30px; text-shadow: 0 2px 3px black;`;
-        this.map.style.cssText = `position: absolute; top: 0px; left: 0px; width: 500px; height: 100px; background-color: #161616; background-size: 100%; background-position: center center; color: #161616; border-radius: 10px; box-shadow: 0px 5px 20px -3px black;`;
-        this.overlay.style.cssText = `position: absolute; top: 0px; left: 0px; width: 500px; height: 100px; background-color: #000; border-radius: 10px; opacity: 40%`;
-        this.bg.style.cssText = `position: absolute; top: 85px; left: 100px; width: 400px; height: 30px; background-color: #161616; color: #fff; border-radius: 15px; box-shadow: 0px 5px 20px -3px black;`;
-        this.stats.style.cssText = `position: absolute; top: 85px; left: 120px; width: 360px; color: #fff; font-family: Exo2; font-size: 15px; line-height: 30px; text-shadow: 0 2px 3px black; text-align: center`;
+        this.pickedStatus.innerHTML = "Picked by Red";
+
+        this.metadata.style.cssText = `position: absolute; top: 30px; left: 20px; width: 500px; color: #fff; font-family: Exo2; font-size: 15px; line-height: 30px; text-shadow: 0 2px 3px black; user-select: none; transition: ease-in-out 200ms;`;
+        this.difficulty.style.cssText = `position: absolute; top: 50px; left: 20px; width: 500px; color: #fff; font-family: Exo2; font-size: 15px; line-height: 30px; text-shadow: 0 2px 3px black; user-select: none; transition: ease-in-out 200ms;`;
+        this.map.style.cssText = `position: absolute; top: 0px; left: 0px; width: 500px; height: 100px; background-color: #161616; background-size: 100%; background-position: center center; color: #161616; border-radius: 10px; box-shadow: 0px 5px 20px -3px black; transition: ease-in-out 200ms;`;
+        this.pickedStatus.style.cssText = `position: absolute; top: 25px; left: 100px; width: 300px; height: 50px; color: #fff; line-height: 50px; font-size: 25px; text-align: center; user-select: none; transition: ease-in-out 100ms; opacity: 0%; border-radius: 25px; text-shadow: 0 0 10px black`;
+        this.overlay.style.cssText = `position: absolute; top: 0px; left: 0px; width: 500px; height: 100px; background-color: #000; border-radius: 10px; opacity: 40%; transition: ease-in-out 200ms;`;
+        this.bg.style.cssText = `position: absolute; top: 85px; left: 100px; width: 400px; height: 30px; background-color: #161616; color: #fff; border-radius: 15px; box-shadow: 0px 5px 20px -3px black; transition: ease-in-out 200ms;`;
+        this.stats.style.cssText = `position: absolute; top: 85px; left: 120px; width: 360px; color: #fff; font-family: Exo2; font-size: 15px; line-height: 30px; text-shadow: 0 2px 3px black; text-align: center; user-select: none; transition: ease-in-out 200ms;`;
+        this.mods.style.cssText = `position: absolute; top: 15px; right: -30px; width: 60px; height: 60px; background-size: 100%; background-image: url("./static/${this.modid}.png"); -webkit-filter: drop-shadow(0px 2px 2px #000); filter: drop-shadow(0px 2px 2px #000); transition: ease-in-out 200ms;`;
         this.clicker.style.cssText = `position: absolute; top: ${this.top}px; left: ${this.left}px; width: 500px; height: 130px; transition: ease-in-out 200ms;`;
 
         clickerObj.appendChild(this.map);
@@ -92,11 +102,15 @@ class Beatmap {
         clickerObj.appendChild(this.difficulty);
         clickerObj.appendChild(this.bg);
         clickerObj.appendChild(this.stats);
+        clickerObj.appendChild(this.mods);
+        clickerObj.appendChild(this.pickedStatus);
     }
     grayedOut() {
         this.overlay.style.cssText = `position: absolute; top: 0px; left: 0px; width: 500px; height: 100px; background-color: #000; border-radius: 10px; opacity: 100%`;
     }
 }
+
+let clickStat = 0;
 
 socket.onmessage = async(event) => {
     let data = JSON.parse(event.data);
@@ -177,13 +191,14 @@ function setupBeatmaps() {
     hasSetup = true;
 
     const bms = [
-        { beatmapId: 3015906, mods: "" },
-        { beatmapId: 1848250, mods: "HR" },
-        { beatmapId: 2994883, mods: "DT" },
-        { beatmapId: 2747949, mods: "HD" },
-        { beatmapId: 2943226, mods: "FM" },
-        { beatmapId: 2931958, mods: "FM" },
-        { beatmapId: 2566810, mods: "FM" },
+        { beatmapId: 3015906, mods: "1" },
+        { beatmapId: 1848250, mods: "2" },
+        { beatmapId: 2994883, mods: "1" },
+        { beatmapId: 2747949, mods: "3" },
+        { beatmapId: 2943226, mods: "1" },
+        { beatmapId: 2931958, mods: "2" },
+        { beatmapId: 2566810, mods: "3" },
+        { beatmapId: 2407203, mods: "2" },
     ]; // For testing only
 
     let row = -1;
@@ -192,11 +207,67 @@ function setupBeatmaps() {
         const bm = new Beatmap(beatmap.mods, beatmap.beatmapId, 130 * row + 50, 500 * (index % 3) + 105 * (index % 3 + 1), `map${index}`);
         bm.generate();
         bm.clicker.onmouseover = function() {
-            bm.clicker.style.transform = "translateY(-10px)";
+            bm.clicker.style.transform = "translateY(-5px)";
         }
         bm.clicker.onmouseleave = function() {
             bm.clicker.style.transform = "translateY(0px)";
         }
+        bm.clicker.addEventListener("mousedown", function() {
+            bm.clicker.addEventListener("click", function(event) {
+                if (event.shiftKey) {
+                    bm.pickedStatus.innerHTML = "Banned by Red";
+                    bm.pickedStatus.style.color = "#de3950";
+                    bm.pickedStatus.style.backgroundColor = "rgba(0, 0, 0, 0)";
+                    bm.pickedStatus.style.top = "25px";
+                    bm.pickedStatus.style.left = "100px";
+                    bm.pickedStatus.style.width = "300px";
+                    bm.pickedStatus.style.height = "50px";
+                    bm.pickedStatus.style.lineHeight = "50px";
+                    bm.pickedStatus.style.fontSize = "25px";
+                    bm.overlay.style.opacity = "80%";
+                    bm.metadata.style.opacity = "30%";
+                    bm.difficulty.style.opacity = "30%";
+                    bm.stats.style.opacity = "0%";
+                    bm.bg.style.opacity = "0%";
+                } else if (event.ctrlKey) {
+                    bm.overlay.style.opacity = "40%";
+                    bm.metadata.style.opacity = "100%";
+                    bm.difficulty.style.opacity = "100%";
+                    bm.stats.style.opacity = "100%";
+                    bm.bg.style.opacity = "100%";
+                    bm.pickedStatus.style.opacity = "0%";
+                } else {
+                    bm.pickedStatus.innerHTML = "Picked";
+                    bm.pickedStatus.style.color = "#fff";
+                    bm.pickedStatus.style.backgroundColor = "#de3950";
+                    bm.pickedStatus.style.top = "85px";
+                    bm.pickedStatus.style.left = "0px";
+                    bm.pickedStatus.style.width = "100px";
+                    bm.pickedStatus.style.height = "30px";
+                    bm.pickedStatus.style.lineHeight = "30px";
+                    bm.pickedStatus.style.fontSize = "15px";
+                    bm.overlay.style.opacity = "40%";
+                    bm.metadata.style.opacity = "100%";
+                    bm.difficulty.style.opacity = "100%";
+                    bm.stats.style.opacity = "100%";
+                    bm.bg.style.opacity = "100%";
+                }
+            });
+            bm.clicker.addEventListener("contextmenu", function(event) {
+                if (event.shiftKey) {
+                    bm.pickedStatus.innerHTML = "Banned by Blue";
+                    bm.pickedStatus.style.color = "#427ef5";
+                    bm.pickedStatus.style.backgroundColor = "rgba(0, 0, 0, 0)";
+                    bm.pickedStatus.style.top = "25px";
+                    bm.pickedStatus.style.left = "100px";
+                    bm.pickedStatus.style.fontSize = "25px";
+                } else {
+                    bm.pickedStatus.innerHTML = "Picked by Blue";
+                    bm.pickedStatus.style.color = "#b0d6ff";
+                }
+            });
+            setTimeout(function() { bm.pickedStatus.style.opacity = "100%"; }, 150);
+        });
         const mapData = await getDataSet(beatmap.beatmapId);
         bm.map.style.backgroundImage = `url('https://assets.ppy.sh/beatmaps/${mapData.beatmapset_id}/covers/cover.jpg')`;
         bm.metadata.innerHTML = mapData.artist + ' - ' + mapData.title;
