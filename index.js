486dd076aa882128cfff7a605aff88411b2fcd94
FukutoTojido <lohoangkhoinguyen1812@gmail.com>
2021-06-07 15:17:00 +0700

    Mung's gamer mode
    api async something idk

--- index.js
+++ index.js
@@ -42,36 +42,16 @@ let gameState;
 let chatLen = 0;
 let tempClass = 'unknown';
 
+let isCalled = false;
+
 /*let bg = document.createElement("bg");
 bg.style.cssText = "position: absolute; top: 0px; left: 0px; width: 1920px; height: 540px; background-color: #fff; color: #161616";
 document.getElementById("main").appendChild(bg);*/
 
-function ObjectReturn(user, data, mapid) {
-    let getUser = async() => {
-        try {
-            const response = await axios.get("/get_beatmaps", {
-                baseURL: "https://osu.ppy.sh/api",
-                params: {
-                    k: `${api}`,
-                    b: `${mapid}`,
-                },
-            });
-            return response.data[0];
-        } catch (error) {
-            console.error(error);
-        }
-    };
-    Promise.resolve(getUser()).then((data) => Object.assign(user, data));
-
-    return user;
-};
-
 class Map {
-    constructor(modid, mapid, width, height, top, left, layerName) {
+    constructor(modid, mapid, top, left, layerName) {
         this.modid = modid;
         this.mapid = mapid;
-        this.width = width;
-        this.height = height;
         this.top = top;
         this.left = left;
         this.layerName = layerName;
@@ -80,23 +60,56 @@ class Map {
     generate() {
         this.bg = document.createElement(this.layerName);
         this.map = document.createElement(`${this.layerName}BG`);
-        this.map.style.cssText = `position: absolute; top: ${this.top}px; left: ${this.left}px; width: ${this.width}px; height: ${this.height}px; background-color: #161616; background-size: 100%; background-position: center center; color: #161616; border-radius: 10px`;
-        this.bg.style.cssText = `position: absolute; top: ${this.top + this.height - 15}px; left: ${this.left + this.width * 0.5}px; width: ${this.width * 0.5}px; height: 30px; background-color: #161616; color: #fff; border-radius: 15px`;
+        this.overlay = document.createElement(`${this.layerName}Overlay`);
+        this.metadata = document.createElement(`${this.layerName}META`);
+        this.difficulty = document.createElement(`${this.layerName}DIFF`);
+        this.metadata.style.cssText = `position: absolute; top: ${this.top + 30}px; left: ${this.left + 20}px; width: 500px; color: #fff; font-family: Exo2; font-size: 15px; line-height: 30px; text-shadow: 0 2px 3px black;`;
+        this.difficulty.style.cssText = `position: absolute; top: ${this.top + 50}px; left: ${this.left + 20}px; width: 500px; color: #fff; font-family: Exo2; font-size: 15px; line-height: 30px; text-shadow: 0 2px 3px black;`;
+        this.map.style.cssText = `position: absolute; top: ${this.top}px; left: ${this.left}px; width: 500px; height: 100px; background-color: #161616; background-size: 100%; background-position: center center; color: #161616; border-radius: 10px; box-shadow: 0px 5px 20px -3px black;`;
+        this.overlay.style.cssText = `position: absolute; top: ${this.top}px; left: ${this.left}px; width: 500px; height: 100px; background-color: #000; border-radius: 10px; opacity: 40%`;
+        this.bg.style.cssText = `position: absolute; top: ${this.top + 85}px; left: ${this.left + 250}px; width: 250px; height: 30px; background-color: #161616; color: #fff; border-radius: 15px; box-shadow: 0px 5px 20px -3px black;`;
         document.getElementById("main").appendChild(this.map);
+        document.getElementById("main").appendChild(this.overlay);
+        document.getElementById("main").appendChild(this.metadata);
+        document.getElementById("main").appendChild(this.difficulty);
         document.getElementById("main").appendChild(this.bg);
     }
 }
 
-let map1 = new Map(1, 2996394, 500, 100, 0, 0, "map1");
+let map1 = new Map(1, 3015906, 50, 105, "map1");
 map1.generate();
-let map2 = new Map(1, 3015906, 500, 100, 500, 500, "map2");
+let map2 = new Map(1, 3021758, 50, 710, "map2");
 map2.generate();
+let map3 = new Map(1, 2994883, 50, 1315, "map3");
+map3.generate();
+let map4 = new Map(1, 2747949, 180, 105, "map4");
+map4.generate();
+let map5 = new Map(1, 2943226, 310, 105, "map5");
+map5.generate();
 
-socket.onmessage = event => {
+socket.onmessage = async(event) => {
     let data = JSON.parse(event.data);
 
-    map1.map.style.backgroundImage = `url('https://assets.ppy.sh/beatmaps/${ObjectReturn(map1.user, data, map1.mapid).beatmapset_id}/covers/cover.jpg')`;
-    map2.map.style.backgroundImage = `url('https://assets.ppy.sh/beatmaps/${ObjectReturn(map2.user, data, map2.mapid).beatmapset_id}/covers/cover.jpg')`;
+    if (!isCalled) {
+        isCalled = true;
+
+        const map1_api = await ObjectReturn(map1.mapid);
+        const map2_api = await ObjectReturn(map2.mapid);
+        const map3_api = await ObjectReturn(map3.mapid);
+        const map4_api = await ObjectReturn(map4.mapid);
+        const map5_api = await ObjectReturn(map5.mapid);
+
+        map1.map.style.backgroundImage = `url('https://assets.ppy.sh/beatmaps/${map1_api.beatmapset_id}/covers/cover.jpg')`;
+        map2.map.style.backgroundImage = `url('https://assets.ppy.sh/beatmaps/${map2_api.beatmapset_id}/covers/cover.jpg')`;
+        map3.map.style.backgroundImage = `url('https://assets.ppy.sh/beatmaps/${map3_api.beatmapset_id}/covers/cover.jpg')`;
+        map4.map.style.backgroundImage = `url('https://assets.ppy.sh/beatmaps/${map4_api.beatmapset_id}/covers/cover.jpg')`;
+        map5.map.style.backgroundImage = `url('https://assets.ppy.sh/beatmaps/${map5_api.beatmapset_id}/covers/cover.jpg')`;
+
+        map1.metadata.innerHTML = map1_api.artist + ' - ' + map1_api.title;
+        map1.difficulty.innerHTML = "Difficulty: " + map1_api.version + '&emsp;&emsp;Mapper: ' + map1_api.creator;
+        map2.metadata.innerHTML = map2_api.artist + ' - ' + map2_api.title;
+        map2.difficulty.innerHTML = "Difficulty: " + map2_api.version + '&emsp;&emsp;Mapper: ' + map2_api.creator;
+    }
     //bg.style.backgroundImage = `url('https://assets.ppy.sh/beatmaps/${ObjectReturn(user, data, 3015906)}/covers/cover.jpg')`;
     //bg.innerHTML = ObjectReturn(user, data, 1848250);
 
@@ -168,4 +181,21 @@ socket.onmessage = event => {
         chats.scrollTop = chats.scrollHeight;
     }*/
 
-}
+};
+
+async function ObjectReturn(mapid) {
+    try {
+        const data = (
+            await axios.get("/get_beatmaps", {
+                baseURL: "https://osu.ppy.sh/api",
+                params: {
+                    k: `${api}`,
+                    b: `${mapid}`,
+                },
+            })
+        )["data"];
+        return data.length !== 0 ? data[0] : null;
+    } catch (error) {
+        console.error(error);
+    }
+};
